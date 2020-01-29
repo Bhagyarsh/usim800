d7ff1589a654dd5b6bd147bd3ffb3b44c63f90af
ThyGM <matteo.corni.studenti@isii.it>
2020-01-29 11:44:21 +0100

    Fixed ports on windows
    Port is now opened a single time and passed as a reference to the other objects

--- usim800/usim800.py
+++ usim800/usim800.py
@@ -3,11 +3,15 @@ from usim800.Sms import sms
 from usim800.Communicate import  communicate
 from usim800.Request import request
 from usim800.Info import info
+import serial
 
 class sim800(communicate):
+    TIMMEOUT = 1
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.requests = request(baudrate=self._baudrate,path=self._path)
-        self.info = info(baudrate=self._baudrate,path=self._path)
-        self.sms = sms(baudrate=self._baudrate,path=self._path)
+        self.port = serial.Serial(path, baudrate=baudrate, timeout=sim800.TIMMEOUT)
+        super().__init__(self.port)
+        self.requests = request(self.port)
+        self.info = info(self.port)
+        self.sms = sms(self.port)
 
