938500bdd3a5f54b608fe25ea81779fda7669bed
john-arvid <jakibsgaard@gmail.com>
2020-04-13 18:09:56 +0200

    Fixed missing variables
    Was a small problem with McLavish's merge. This solves it but please check if there is a better way to fix it.

--- usim800/usim800.py
+++ usim800/usim800.py
@@ -8,8 +8,8 @@ import serial
 class sim800(communicate):
     TIMMEOUT = 1
 
-    def __init__(self, *args, **kwargs):
-        self.port = serial.Serial(path, baudrate=baudrate, timeout=sim800.TIMMEOUT)
+    def __init__(self, baudrate, path):
+        self.port = serial.Serial(path, baudrate, timeout=sim800.TIMMEOUT)
         super().__init__(self.port)
         self.requests = request(self.port)
         self.info = info(self.port)
